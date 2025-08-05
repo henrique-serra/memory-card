7ae5252cca0790daaa519f90a5c8501ffc681709
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-08-05 21:02:29 +0000

    setup react and chakra

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+const App = () => {
+  return (
+    <HStack>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Button>Button</Button>
+    </HStack>
+  )
+}
+
+export default App
